e336a1a9b969845b53c1ae7176bf654c3b5fcd1b
Yuki Okawa <okawa.yuki@nifty.co.jp>
2016-09-08 18:44:04 +0900

    Fixed chart sample to create image of line chart

--- chart_sample.js
+++ chart_sample.js
@@ -9,7 +9,8 @@ co(function*() {
   const html = '<html><head><script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script><script type="text/javascript">google.charts.load("current", {"packages":["corechart"]});</script></head><body><div id="chart"><p>test</p></div></body></html>';
   page.setContent(html, "");
   page.on("onConsoleMessage", function(msg){
-    console.log(msg);
+    //console.log(msg);
+    page.render('chart.jpeg', {format: 'jpeg', quality: '100'});
     page.close();
     instance.exit();
   });
@@ -21,21 +22,22 @@ co(function*() {
       google.charts.setOnLoadCallback(function (){
         console.log(JSON.stringify(google.charts));
         var data = google.visualization.arrayToDataTable([
-          ['Element', 'Density', { role: 'style' }],
-          ['Copper', 8.94, '#b87333', ],
-          ['Silver', 10.49, 'silver'],
-          ['Gold', 19.30, 'gold'],
-          ['Platinum', 21.45, 'color: #e5e4e2' ]
+          ['Date', 'Completed task number'],
+          ['9/1', 8],
+          ['9/2', 10],
+          ['9/3', 19],
+          ['9/4', 21]
         ]);
 
         var options = {
-          title: "Density of Precious Metals, in g/cm^3",
-          bar: {groupWidth: '95%'},
+          title: "Line chart of Completed task number",
           legend: 'none',
+          width: 600,
+          height: 300
         };
 
         var chart_div = document.getElementById('chart');
-        var chart = new google.visualization.ColumnChart(chart_div);
+        var chart = new google.visualization.LineChart(chart_div);
 
         // Wait for the chart to finish drawing before calling the getImageURI() method.
         google.visualization.events.addListener(chart, 'ready', function () {
